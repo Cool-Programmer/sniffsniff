4b3851325558f85f780a812f222cc20fa0e15ab1
= <margaryan.mher.28@gmail.com>
2016-11-14 20:37:27 +0400

    Sorry.

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ import socket
 import struct
 import textwrap
 
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
+
+DATA_TAB_1 = '\t '
+DATA_TAB_2 = '\t\t '
+DATA_TAB_3 = '\t\t\t '
+DATA_TAB_4 = '\t\t\t\t '
 
 def main():
     conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
@@ -17,6 +26,12 @@ def main():
         print('\n Ethernet protocol')
         print('Destination: {}, Source: {}, Protocol: {} ' . format(dest_mac, src_mac, eth_proto))
 
+        # Check if using regular ethernet protocol 8
+        if eth_proto == 8:
+            (version, header_length, ttl, proto, src, target, data) = ipv4_packet(data)
+            print(TAB_1 + 'IPv4 Packet: ')
+            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {}' . format(version, header_length, ttl))
+            print(TAB_3 + 'Protocol: {}, Source: {}, Target: {}'.format(proto, src, target))
 
 
 # Unpack ethernet frame
@@ -29,5 +44,52 @@ def ethernet_frame(data):
 def get_mac_address(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_addr).upper()
+
+# Unpack ipv4 packet
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return  version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+# Format ipv4 address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+# Unpack ICMP packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+# Unpack TCP segment
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknoledgement, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fyn = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknoledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fyn, data[offset:]
+
+# Unpack UDP segment
+def udp_segment(data):
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
+
+# Format multi-line data
+def format_multi_line(prefix, string, size = 80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return  '\n' . join([prefix + line for line in textwrap.wrap(string, size)])
+
+
+
+
 
 main()
